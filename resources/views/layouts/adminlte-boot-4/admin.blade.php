fd49e614fa10311758f004a0320b0c03c3dfd694
Neeraj1005 <neeraj.tangariya123@gmail.com>
2021-04-14 18:45:32 +0530

    favicon added

--- resources/views/layouts/adminlte-boot-4/admin.blade.php
+++ resources/views/layouts/adminlte-boot-4/admin.blade.php
@@ -9,6 +9,7 @@
     <meta name="description" content="Digital CRM">
     <meta name="keywords" content="Digital CRM">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('public/bigindiafav.png') }}">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="{{asset('assets/adminlte-boot-4/plugins/fontawesome-free/css/all.min.css')}}">
     {{-- google source https://www.bootstrapcdn.com/fontawesome/ --}}
